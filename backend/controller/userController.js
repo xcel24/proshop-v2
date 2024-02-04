1ab759cd9bbadf7c980f80811c570253dc17743e
xcel24 <prateekgoswami1996@gmail.com>
2024-02-04 12:59:44 +0530

    completed backend authentication

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from '../middleware/asyncHandler.js'
 import User from '../models/userModel.js'
-import jwt from 'jsonwebtoken'
+
+import { generateToken } from '../utils/generateToken.js'
 
 //@desc - Auth user and get token
 //@route - POST /api/users/login
@@ -11,18 +12,9 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    })
+    generateToken(res, user._id)
 
-    //set jwt as http-only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
-    })
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -38,28 +30,95 @@ const authUser = asyncHandler(async (req, res) => {
 //@route - POST /api/users
 //@access - public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user')
+  const { name, email, password } = req.body
+
+  const userExist = await User.findOne({ email })
+
+  if (userExist) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    generateToken(res, user._id)
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 //@desc - Logout user and clear cookie
 //@route - POST /api/users/logout
 //@access - Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('logout user')
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  })
+  res.status(200).json({ message: 'Logged Out Successfully' })
 })
 
 //@desc - Get user profile
 //@route - GET /api/users/profile
 //@access - Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('get user profile')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    const { _id, name, email, isAdmin } = user
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('Profile not found')
+  }
 })
 
 //@desc - Update user profile
 //@route - PUT /api/users/profile
 //@access - Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send('update user profile')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    ;(user.name = req.body.name || user.name),
+      (user.email = req.body.email || user.email)
+
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    const { _id, name, email, isAdmin } = updatedUser
+
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('No user found')
+  }
 })
 
 //@desc - Get all users
